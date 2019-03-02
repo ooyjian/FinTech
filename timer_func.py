98c008334c975e92da08d93f8063c60f07d815bb
Aelo14 <ouyujian14@gmail.com>
2019-03-02 11:37:47 -0600

    Timer Function Update

--- timer_func.py
+++ timer_func.py
@@ -3,11 +3,17 @@ import time
 def time_func(interval):
     """
     Send the information about stock price every interval seconds
-    :param interval: the length of the interval in seconds
+    :param interval: a tuple that is in the form of ('time length', 'time unit')
     :return: None
     """
-    for i in range(3):
+    # Compute how many seconds are there in the interval
+    time_length = float(interval[0])
+    if interval[1] == str.capitalize("hours") or interval[1] == str.capitalize("hour"):
+        interval = time_length * 3600
+    elif interval[1] == str.capitalize("minutes") or interval[1] == str.capitalize("minute"):
+        interval = time_length * 60
+    else:
+        interval = time_length
+    while True:
         time.sleep(interval)
         print("Hello World!")
-
-time_func(30)
