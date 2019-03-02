d1ea57cbba78bda6d10c5ea911c9a0816602d1f7
Xin Hao <42793737+Xin128@users.noreply.github.com>
2019-03-02 13:45:59 -0600

    Add files via upload

---
+++ Tech_company.py
@@ -0,0 +1,4 @@
+company_graph = {'Apple':'AAPL', 'Amazon':'AMZN', 'Facebook':'FB', 'Google':'GOOGL', 'IBM':'IBM',
+                 'Microsoft':'MSFT', 'Intel':'INTC','Compass':'CMPGY','Adobe':'ADBE','SAP':'SAP',
+                 'ZOOM':'ZOOM','Samsung':'SSNLF','Hon Hai Precision':'2354','TENCENT':'TCEHY',
+                 'Sony':'SNE','Oracle':'ORCL','Tesla':'TSLA','Alibaba':'BABA','Baidu':'BIDU'}
